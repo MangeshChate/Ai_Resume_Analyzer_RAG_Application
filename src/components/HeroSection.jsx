cca8f9b9f01d83fc0c77b155b07cc30740a718ff
mangesh <mbchate08@gmail.com>
2024-08-19 17:24:05 +0530

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => (
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="text-4xl font-bold mb-4"
+        className="text-4xl font-extrabold mb-4 "
       >
         Effortlessly Analyze Your Resume
       </motion.h2>
@@ -16,7 +16,7 @@ const HeroSection = () => (
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        className="mb-8"
+        className="mb-8 text-sm"
       >
         Upload your resume and get instant, personalized feedback on your strengths and areas for improvement.
       </motion.p>
